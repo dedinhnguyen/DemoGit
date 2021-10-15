6b826bf3964731649db04c47d96a87c7c68fe9a6
De Nguyen <dinhde1221@gmail.com>
2021-10-15 08:04:46 +0700

    them tac gia, nam, giay phep

--- Hellogit.java
+++ Hellogit.java
@@ -1,6 +1,8 @@
 public class Hellogit{
     public static void main(String[] args){
         System.out.println("Welcome to version control System with Git");
-
+        System.out.println("Author: Linus Torvald");
+        System.out.println("Year: 2005");
+        System.out.println("license: GPL Version 2.0");
     }
 }
